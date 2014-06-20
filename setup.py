8c20283dde16fcbbda81fbf3a8e868945f1e5ffb
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2014-06-20 08:30:36 +0000

    Moved package sources to src, minor fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='nibbler',
     version='1.0',
     description='ZMQ-Protobuf Websocket Bridge',
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={'nibbler': ['static/*']},
     scripts=['scripts/nibbler'],
     setup_requires=['nose>=1.0'],
     install_requires=['pyzmq', 'protobuf', 'cherrypy', 'ws4py', 'docopt']
